328bb5bee9610360bc740a3175d2524cc77b448f
siavosh <siavoshrahimir@gmail.com>
2024-08-12 12:18:31 +0200

    Adding Spinner-changing style in Bobble,Events

--- src/components/bubble/Bubble.js
+++ src/components/bubble/Bubble.js
@@ -2,8 +2,14 @@ import React from "react";
 
 const Bubble = ({ message, isUser }) => {
   return (
-    <div className={`chat ${isUser ? "chat-end" : "chat-start"}`}>
-      <div className={`chat-bubble ${isUser ? "text-neutral-200	" : "text-cyan-600"}`}>{message}</div>
+    <div className={`chat pb-16 ${isUser ? "chat-end" : "chat-start"}`}>
+      <div
+        className={`chat-bubble p-4  ${
+          isUser ? "text-neutral-200 lg:w-3/4 " : "text-cyan-600 lg:w-3/4"
+        }`}
+      >
+        {message}
+      </div>
     </div>
   );
 };
